f56c431cb57cc303bd741572b58debc2cfcc4508
mnoorlander98 <m.noorlander98@gmail.com>
2025-10-05 12:36:37 +0100

    first code from example

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is just a newsripper to improve my python skills
+This is just a newsripper to improve my python skills using a rss feed to get articles
 I wil initially use the guardian but maybe i can add different news sources later
 
--- newsripper.py
+++ newsripper.py
@@ -2,6 +2,34 @@
 
 import newspaper
 import feedparser
-print ('done')
-#def scrape_news_from_feed(feed url):
-#    articles= []
+
+
+def scrape_news_from_feed(feed_url):
+    articles= []
+    feed = feedparser.parse(feed_url)
+    for entry in feed.entries:
+        # create a newspaper article object
+        article = newspaper.Article(entry.link)
+        # download and parse the article
+        article.download()
+        article.parse()
+        # extract relevant information
+        articles.append({
+            'title': article.title,
+            'author': article.authors,
+            'publish_date': article.publish_date,
+            'content': article.text
+        })
+    return articles
+
+
+feed_url='https://www.theguardian.com/uk/rss'
+articles = scrape_news_from_feed(feed_url)
+
+#print extracted articles
+for article in articles:
+    print('Title:', article['title'])
+    print('Author:', article['author'])
+    print('Publish Date:', article['publish_date'])
+    print('Content:', article['content'])
+    print()
